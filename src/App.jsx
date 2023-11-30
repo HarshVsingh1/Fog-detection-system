11da18e2fb6e3d73b256d94fb68d6751074eaa83
ManavRastogi03 <manavrastogi2003@gmail.com>
2023-11-30 10:49:10 +0530

    Home-Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
-
+import Nav from './component/Nav'
+import About from './component/About'
+import Footer from './component/Footer'
+import List from './component/list'
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Nav/>
+      <About/>
+      <List/>
+      <Footer/>
     </>
   )
 }
---
+++ src/component/List.jsx
@@ -0,0 +1,40 @@
+import React, { PureComponent } from 'react'
+
+function List() {
+    return (
+        <>
+        <div className=' mb-4 ml=4'> 
+            <h1 class="mb-2 text-lg font-semibold text-gray-900 dark:text-red ml-2">Key Points Of Your Site</h1>
+            <ul class="max-w-md space-y-1 text-purple-500 list-inside dark:text-purple-400 ml-9 text-xl font-extrabold">
+                <li class="flex items-center">
+                    <svg class="w-3.5 h-3.5 me-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+                    </svg>
+                  <h1>  Enhanced Safety:</h1>
+    </li>
+    <li class="flex items-center">
+        <svg class="w-3.5 h-3.5 me-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+         </svg>
+         Reduced Accidents and Delays:
+    </li>
+    <li class="flex items-center">
+        <svg class="w-3.5 h-3.5 me-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+         </svg>
+         Improved Traffic Flow:
+    </li>
+    <li class="flex items-center">
+        <svg class="w-3.5 h-3.5 me-2 text-gray-500 dark:text-gray-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+         </svg>
+         Time Spending:
+    </li>
+</ul>
+</div> 
+
+        </>
+        )
+    }
+    
+    export default List
